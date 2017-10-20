f6d01895502de32deacc1f3fdcbd7c51b0ab7695
Ildar Gabdrakhmanov <ildar@lentaserver.localdomain>
2017-10-20 07:49:34 +0700

    dbpostgres added

---
+++ dbpostgres.R
@@ -0,0 +1,41 @@
+require("RPostgreSQL")
+
+drv <- dbDriver('PostgreSQL')  
+db <- 'ildar'  
+host_db <- 'localhost'  
+db_port <- '5432'  
+db_user <- 'ildar'  
+db_password <- 'ghtdtl'
+
+con <- dbConnect(drv, dbname=db, host=host_db, port=db_port, user=db_user, password=db_password)
+
+dbListTables(con)
+
+sql_command <- "CREATE TABLE cartable
+(
+  carname character varying NOT NULL,
+  mpg numeric(3,1),
+  cyl numeric(1,0),
+  disp numeric(4,1),  
+  hp numeric(3,0),
+  drat numeric(3,2),
+  wt numeric(4,3),
+  qsec numeric(4,2),
+  vs numeric(1,0),
+  am numeric(1,0),
+  gear numeric(1,0),
+  carb numeric(1,0),
+  CONSTRAINT cartable_pkey PRIMARY KEY (carname)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE cartable
+  OWNER TO ildar;
+COMMENT ON COLUMN cartable.disp IS '
+';"
+# sends the command and creates the table
+dbGetQuery(con, sql_command)
+
+dbDisconnect(con)
+dbUnloadDriver(drv)
--- temp.R
+++ temp.R
@@ -10,5 +10,25 @@ archivePagesLinks <- paste0(baseURL, "/", year(dayArray),
   "/", formatC(day(dayArray), width = 2, format = "d", flag = "0"), 
   "/")
 
+
+# create a connection
+# save the password that we can "hide" it as best as we can by collapsing it
+pw <- {
+  "ghtdtl"
+}
+
+# loads the PostgreSQL driver
+drv <- dbDriver("PostgreSQL")
+# creates a connection to the postgres database
+# note that "con" will be used later in each connection to the database
+con <- dbConnect(drv, dbname = "ildar",
+                 host = "localhost", port = 5432,
+                 user = "ildar", password = "ghtdtl")
+rm(pw) # removes the password
+
+# check for the cartable
+dbExistsTable(con, "cartable")
+
+
 #dayscollection$find('{ "$or": [ { "b": 6 }, { "c": 10 } ] }')
 #dayscollection$find('{ "b": { "$in" : [ 6, 8, 10]  } }')
